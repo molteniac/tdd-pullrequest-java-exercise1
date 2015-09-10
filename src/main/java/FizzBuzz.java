0d964c9b2394b080eaedd7fcbfd5d88b63a19a01
eiji.ienaga <eiji.ienaga@gmail.com>
2015-09-10 13:17:51 +0900

    エクササイズ１をつくった

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,9 @@
+public class FizzBuzz {
+
+    // TODO 2.リファクタリング 変数名
+    // TODO 3.リファクタリング メソッド名 (option)
+    public String say(int hoga) {
+        return String.valueOf(hoga);
+    }
+}
+// TODO これは不要コメント
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
+public class FizzBuzzTest {
+
+    // TODO 4. 3の倍数場合はFizzを返すこと
+    // TODO 5. 5の倍数場合はBuzzを返すこと
+    // TODO 6. 3と5の倍数場は合FizzBuzzを返すこと
+    // TODO 7. 1から100の数列をfizzbuzz配列に変換すること
+
+    public static class _3の倍数でも5の倍数でもない場合 {
+        FizzBuzz fizzbuss;
+
+        @Before
+        public void setUp() {
+            fizzbuss = new FizzBuzz();
+        }
+
+        @Test
+        public void 数字を返すこと_2() {
+            assertEquals("2", fizzbuss.say(2));
+        }
+        // TODO 1. 数値2以外のケースでも期待通り数字返すことを確認
+    }
+}

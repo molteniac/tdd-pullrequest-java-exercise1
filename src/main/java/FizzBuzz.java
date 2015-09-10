b5ce93d2f7919cd45b9f7dcaabb0a4f18c514dd7
eiji.ienaga <eiji.ienaga@gmail.com>
2015-09-10 15:32:15 +0900

    ワークの仕込みを追加

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,7 +3,7 @@ public class FizzBuzz {
     // TODO 2.リファクタリング 変数名
     // TODO 3.リファクタリング メソッド名 (option)
     public String say(int hoga) {
-        return String.valueOf(hoga);
+        return String.valueOf(2);
     }
 }
 // TODO これは不要コメント
